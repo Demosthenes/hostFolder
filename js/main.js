797920c1fc8baf4d951fe4d83ea76834a7106d70
MikaelHellsen <mikael.hellsen@expereo.com>
2021-02-11 19:07:37 +0100

    improved html index

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ import myHostFolder from './myHostFolder.js'
 
 $(document).ready(function () {
     // Initiate custom hostFolder
-    myHostFolder($('#gallery'));
+    let folder = myHostFolder($('#gallery'));
 });
