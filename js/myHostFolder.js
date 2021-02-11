39382f443fc239d947e9994f75a15478a14fdf2d
MikaelHellsen <mikael.hellsen@expereo.com>
2021-02-11 19:11:41 +0100

    improved html index

--- js/myHostFolder.js
+++ js/myHostFolder.js
@@ -16,41 +16,8 @@ export default function myHostFolder ($container) {
         $container
     });
 
-    // What happens once the data is changed?
-    folder.textCallback = (id, text) => {
-        console.log("text changed: " + text)
-
-        // Default code to handle results
-        if (folder.noContainer()) return false;
-        if ($(`#hostFolder_${id}`).length) {
-            $(`#hostFolder_${id} .host-folder-text`).text(text).removeClass('d-none')
-        } else {
-            $container.append(`
-                <div class="col-md-3" id="hostFolder_${id}">
-                    <p class="host-folder-text">${text}</p>
-                    <img class="host-folder-image d-none img-fluid">
-                </div>`)
-        }
-    }
-
-    folder.imageCallback = (id, url) => {
-        if (folder.noContainer()) return false;
-        $(`#hostFolder_${id} .host-folder-image`).attr('src', url).removeClass('d-none')
-      }
-
-    folder.textCompleted  = (results, total) => {
-        if (folder.noContainer()) return false;
-        $(`#hostFolder_${results[total-1].id + 1}`).remove();
-    }
-
-    folder.imageCompleted = (results, total) => {
-        console.log(`Fetched all data from ${total} entries`)
-        console.log(results)
-    }
-
-    // load the content and save it into results
-    // lazy loaded images will get updated once server responds and can be found in folder.results
-    let results = folder.load(); 
+    // load the content
+    folder.load(); 
     
     return folder;
 }
